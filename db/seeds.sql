ad8cd86ba0228f73956c78e1c0ac13ff369f0cf9
mcheung008 <mcheung008@gmail.com>
2021-01-03 16:19:13 -0800

    updates, still not working

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Chili burger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Red Robin burger');
